c17c944591e12b7e26e36fae2141d9ed1005af97
bigdaddyjean <jeannappoleon@pursuit.org>
2022-11-16 19:44:18 -0500

    header

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect } from 'react';
+import "./App.css";
+import { useEffect } from "react";
+import Header from "./Components/Header";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  
-  useEffect(()=>{
-    console.log(process.env.REACT_APP_API_KEY)
-    fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}`)
-    .then(response=>response.json())
-    .then(data=>{console.log(data)})
-    .error(error=>console.log(error));
-  },[]);
-  
+  useEffect(() => {
+    console.log(process.env.REACT_APP_API_KEY);
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header></Header>
+      </div>
+    </BrowserRouter>
   );
 }
 
