e50ccb88ae4b8c57214e7b8245a00ae3b975121a
hector <52504713+hector918@users.noreply.github.com>
2022-11-24 16:50:36 +0000

    add search feature with next page, but without the css, duo to the api is out of quota, I have to debug it tmr

--- src/Components/Home/Searchbar.js
+++ src/Components/Home/Searchbar.js
@@ -3,43 +3,32 @@ import style from "./index.module.css";
 // import { useNavigate } from "react-router-dom";
 import {decode as base64_decode, encode as base64_encode} from 'base-64';
 // import { useErrorHandler } from 'react-error-boundary';
+let _SetVideos;
 
 export default function Searchbar({ query, setVideos }) {
-  
-  const search_default_setting = {q:"","maxResults":"5" };
+  ////variable/////////////////////////////////
+  _SetVideos = setVideos;
+  const search_default_setting = {q:"","maxResults":"5",order:"date",safeSearch:"none",relevanceLanguage:"en"};
   const [search,setSearch] = useState(query?.slice(8)||"");
-
+  const [pageTokens, SetPageTokens] = useState([]);
+  const [searchKeyword, setSearchKeyword] = useState(search_default_setting);
+  ///////////////////////////////////////////////////////////////////////
   useEffect(()=>{
-    console.log(search);
-    const decode_search = `https://youtube.googleapis.com/youtube/v3/search?q=${base64_decode(search)}part=snippet&type=video&key=${process.env.REACT_APP_API_KEY}`;
-    console.log(decode_search);
-    
-
-    fetch(decode_search)
+    const decode_search = base64_decode(search);
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${decode_search}&part=snippet&type=video&key=${process.env.REACT_APP_API_KEY}`)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-        if(data['error']) console.log(data['error'])
-        console.log(data["result"]);
+
+        // console.log(data);
+        if(data['error']) console.log(data['error']);
+        if(_SetVideos) _SetVideos(data["items"]);
+        if(data["nextPageToken"]) SetPageTokens(pv=>[...pv,data["nextPageToken"]]);
       })
       .catch((error) => {
-        console.log("123",error);
+        //should add a error handling in here
+        console.log(error);
       })
-    // try {
-    //   fetch(
-    //     `https://youtube.googleapis.com/youtube/v3/search?q=${decode_search}part=snippet&type=video&key=${process.env.REACT_APP_API_KEY}`
-    //   )
-    //     .then((response) => response.json())
-    //     .then((data) => {
-    //       setVideos(data.items);
-    //     })
-    //     .catch((error) => {
-    //       console.log("123",error);
-  
-    //     })
-    // } catch (error) {
-    //   console.log("123",error);
-    // }
+
     
   },[search])
 
@@ -47,9 +36,17 @@ export default function Searchbar({ query, setVideos }) {
   //handle window histroy back and forward event
   window.onpopstate = function(event) {
     console.log("location: " + document.location + ", state: " + JSON.stringify(event.state));
+    console.log(search);
+    const decode_search = base64_decode(search);
+    const keyword_filter = Object.keys(search_default_setting);
+    for(let x of decode_search.split("&"))
+    {
+      let [ key , val ] = x.split("=");
+      if(keyword_filter.includes(key)) setSearchKeyword(pv=>({...pv,[key]:val}));
+    }
   };
-  //
-  const [searchKeyword, setSearchKeyword] = useState(search_default_setting);
+  /////event//////////////////////////////////////////////////////////////
+  
   // const navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -62,10 +59,16 @@ export default function Searchbar({ query, setVideos }) {
     var url = `${window.location.pathname}?results=${search_para}`;
     window.history.pushState({url: url}, '', url);
     setSearch(search_para);
+    SetPageTokens([]);
+    console.log(window.history);
     // navigate(`/results=${base64_encode(JSON.stringify(searchKeyword))}`)
   };
-
-
+  const on_page_button_click= (evt)=>{
+    const dirction = evt.target.name==="next"?pageTokens.pop():"";
+    const decode_search = base64_decode(search);
+    setSearch(base64_encode(`${decode_search}&pageToken=${dirction}`));
+  }
+/////////////////////////////////////////////////////////////////////////////////
   return (
     <div>
       <form className={style.form} onSubmit={handleSubmit}>
@@ -81,19 +84,21 @@ export default function Searchbar({ query, setVideos }) {
         {/* from hector I add code below from search feature */}
         <div>
           <label>number of content per page
-            <select name="maxResults"><option>5</option><option>10</option><option>15</option></select>
+            <select name="maxResults" value={searchKeyword.maxResults} onChange={(e) => setSearchKeyword(pv=>({...pv,[e.target.name]:e.target.value}))}><option>5</option><option>10</option><option>15</option></select>
           </label>
           <label>Order
-            <select name="order"><option>date</option><option>rating</option><option>relevance</option><option>title</option><option>videoCount </option><option>viewCount </option></select>
+            <select name="order" value={searchKeyword.order} onChange={(e) => setSearchKeyword(pv=>({...pv,[e.target.name]:e.target.value}))}><option>date</option><option>rating</option><option>relevance</option><option>title</option><option>videoCount </option><option>viewCount </option></select>
           </label>
           <label>
-            <select name="safeSearch"><option>none</option><option>moderate</option><option>strict</option></select>
+            <select name="safeSearch" value={searchKeyword.safeSearch} onChange={(e) => setSearchKeyword(pv=>({...pv,[e.target.name]:e.target.value}))}><option>none</option><option>moderate</option><option>strict</option></select>
           </label>
           <label>Language
-            <select name="relevanceLanguage"><option>en</option><option>fr</option><option>zh</option></select>
+            <select name="relevanceLanguage" value={searchKeyword.relevanceLanguage} onChange={(e) => setSearchKeyword(pv=>({...pv,[e.target.name]:e.target.value}))}><option>en</option><option>fr</option><option>zh</option></select>
           </label>
         </div>
       </form>
+      <div><button name="prev" onClick={on_page_button_click}>first page</button><button  name="next" onClick={on_page_button_click}>next page</button></div>
+      
     </div>
   );
 }
